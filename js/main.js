b0a413e8d9aea4d3891df7119ffb2b24164a99a5
KLPainter <KLPainter@gmail.com>
2018-11-09 17:02:55 -0800

    proof of concept

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 function resizeIframe(obj) {
     obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
 }
+
+function switchPointers(target) {
+    const nav = document.getElementsByTagName('nav');
+    const spans = nav[0].getElementsByTagName('span');
+    for(i = 0; i < spans.length; i++) {
+        if(spans[i].id === `span-${target}`) {
+            spans[i].classList.remove('hidden');
+            spans[i].classList.add('visible');
+        } else {
+            spans[i].classList.remove('visible');
+            spans[i].classList.add('hidden');
+        }
+    }
+}
